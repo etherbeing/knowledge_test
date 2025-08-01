edbfb8879193f5b7e3f92bc35a786af749a93f28
Esteban Chacon Martin <etherbeing99@proton.me>
2025-08-01 19:50:08 -0400

    fix: Using UV and removing old poetry deps as poetry is not as fast as UV :-)

---
+++ api/core/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# mkdir -p /var/log/celery/
+
+# python3 -m celery  -A todo beat --logfile=/var/log/celery/beat.log --loglevel=info --pidfile=/tmp/celeryworker.pid --schedule=/tmp/celerybeat-schedule --detach
+# python3 -m celery  -A todo worker --logfile=/var/log/celery/worker.log --loglevel=info --pidfile=/tmp/celerybeat.pid --schedule=/tmp/celerybeat-worker --detach
+
+gunicorn todo.wsgi:application --bind :8000 --workers 4 --timeout 20
