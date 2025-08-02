e3d25ac7313e8a46b6ebbce9594a48b075dc7a3d
Esteban Chacon Martin <etherbeing99@proton.me>
2025-08-01 22:21:12 -0400

    fix: Create DB after volume is mounted on enrypoint to avoid losing the DB

--- api/core/entrypoint.sh
+++ api/core/entrypoint.sh
@@ -4,4 +4,5 @@
 # python3 -m celery  -A todo beat --logfile=/var/log/celery/beat.log --loglevel=info --pidfile=/tmp/celeryworker.pid --schedule=/tmp/celerybeat-schedule --detach
 # python3 -m celery  -A todo worker --logfile=/var/log/celery/worker.log --loglevel=info --pidfile=/tmp/celerybeat.pid --schedule=/tmp/celerybeat-worker --detach
 
+uv run python ./manage.py migrate
 uv run gunicorn todo.wsgi:application --bind :8000 --workers 4 --timeout 20
